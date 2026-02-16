e315b4a41ab2e71cf28f63f9f465b108d7b35391
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-17 06:59:44 +0700

    Refactor user service file structure

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -21,5 +21,4 @@ exports.findAll = async () => repo.findAll();
 exports.findById = async (id) => repo.findById(id);
 exports.create = async (data) => repo.create(data);
 exports.update = async (id, data) => repo.update(id, data);
-
 exports.remove = async (id) => repo.remove(id);
