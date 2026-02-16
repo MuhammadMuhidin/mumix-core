eed8d4bf8f9dd10378a7fe14a0e3596a4376bccd
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-17 06:45:41 +0700

    Enhance user service with documentation comments
    Added documentation comments to the user service functions.

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -1,7 +1,25 @@
 const repo = require("./user.repository");
 
+/**
+ * USER SERVICE
+ *
+ * Scope:
+ * - Menjalankan logika bisnis.
+ * - Menentukan aturan domain (misalnya user harus ada, email unik, dsb).
+ * - Mengatur alur proses sebelum/ sesudah akses data.
+ * - Memanggil repository.
+ *
+ * Tidak boleh:
+ * - Mengakses req/res.
+ * - Menentukan format response HTTP.
+ *
+ * Role:
+ * Pusat keputusan dan aturan sistem.
+ */
+
 exports.findAll = async () => repo.findAll();
 exports.findById = async (id) => repo.findById(id);
 exports.create = async (data) => repo.create(data);
 exports.update = async (id, data) => repo.update(id, data);
+
 exports.remove = async (id) => repo.remove(id);
