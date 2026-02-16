968a74b5fbc5ee14204d235012258aa0066cdc12
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-17 06:43:26 +0700

    Add documentation to user route
    Added documentation comments to clarify the purpose and limitations of the user route.

--- backend/src/modules/user/user.route.js
+++ backend/src/modules/user/user.route.js
@@ -4,10 +4,28 @@ const ctrl = require("./user.controller");
 const { validate } = require("../../middlewares/validate.middleware");
 const validator = require("./user.validator");
 
+/**
+ * USER ROUTE
+ *
+ * Scope (Apa yang boleh dilakukan):
+ * - Mendefinisikan endpoint HTTP (GET, POST, PUT, DELETE).
+ * - Menghubungkan route ke validator dan controller.
+ * - Menentukan urutan middleware.
+ *
+ * Tidak boleh:
+ * - Mengandung logika bisnis.
+ * - Mengakses database.
+ * - Mengolah data.
+ *
+ * Role:
+ * Route hanya bertugas sebagai wiring layer (penghubung).
+ */
+
 router.get("/", ctrl.findAll);
 router.get("/:id", ctrl.findById);
 router.post("/", validate(validator.create), ctrl.create);
 router.put("/:id", ctrl.update);
 router.delete("/:id", ctrl.remove);
+
 
 module.exports = router;
